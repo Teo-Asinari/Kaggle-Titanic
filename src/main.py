368229bdb0598232b1e9311894841a55904d3b63
Teo Asinari <tasinari@gmail.com>
2021-05-25 13:38:22 -0700

    Cleanup for OS change

--- src/main.py
+++ src/main.py
@@ -14,4 +14,11 @@ from tensorflow.keras import losses
 from tensorflow.keras import preprocessing
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
-print(tf.__version__)
+batch_size=32
+seed=42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory('dataset/',
+                                                                  batch_size=batch_size,
+                                                                  validation_split=0.2,
+                                                                  subset='training',
+                                                                  seed=seed)
